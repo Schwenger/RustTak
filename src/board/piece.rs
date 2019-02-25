9a1a9474c03c4d2b010952daa3c161120b5ca16c
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2019-02-25 18:41:41 +0100

    Revised pieces and stacks.

--- src/board/piece.rs
+++ src/board/piece.rs
@@ -1,6 +1,7 @@
 
 use crate::player::Color;
 use std::ops::{AddAssign, SubAssign};
+use crate::board::piece::PieceKind::CapStone;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Piece {
@@ -55,8 +56,45 @@ pub struct Stack {
 
 impl Stack {
 
+    pub fn nth_piece(&self, n: usize) -> Piece {
+        self.content[self.len() - 1 - n]
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<Piece> {
+        self.content.iter()
+    }
+
     pub(crate) fn empty() -> Stack {
         Stack { content: Vec::new() }
+    }
+
+    pub(crate) fn is_road(&self) -> bool {
+        self.top().map(|p| p.kind.road()).unwrap_or(false)
+    }
+
+    pub(crate) fn is_empty(&self) -> bool {
+        self.content.is_empty()
+    }
+
+    pub(crate) fn compatible_with(&self, other: &Stack) -> bool {
+        self.is_stackable() || self.is_flattenable() && other.is_flattening()
+    }
+
+    pub(crate) fn take_off(&mut self, n: usize) -> Stack {
+        self.content.split_off(self.content.len() -  n).into()
+    }
+
+    pub(crate) fn peek_from_top(&self, n: usize) -> Stack {
+        let new_content = self.content.split_at(self.content.len() - n).1;
+        Vec::from(new_content).into()
+    }
+
+    pub(crate) fn color(&self) -> Option<Color> {
+        self.top().map(|p| p.color)
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.content.len()
     }
 
     fn top(&self) -> Option<&Piece> {
@@ -65,10 +103,6 @@ impl Stack {
 
     fn is_atomic(&self) -> bool {
         self.content.len() == 1
-    }
-
-    pub(crate) fn is_empty(&self) -> bool {
-        self.content.is_empty()
     }
 
     fn is_flattening(&self) -> bool {
@@ -83,10 +117,6 @@ impl Stack {
         self.top().map(|p| p.kind.flattenable()).unwrap_or(true)
     }
 
-    pub(crate) fn is_road(&self) -> bool {
-        self.top().map(|p| p.kind.road()).unwrap_or(false)
-    }
-
     fn is_stackable(&self) -> bool {
         self.top().map(|p| p.kind.stackable()).unwrap_or(true)
     }
@@ -98,63 +128,47 @@ impl Stack {
                 _ => return, // Short-circuit-ish
             };
             *self -= 1;
-            *self += new_top;
+            *self += new_top; // TODO: We do not need to flatten again here.
         }
     }
 
-    pub(crate) fn compatible_with(&self, other: &Stack) -> bool {
-        self.is_stackable() || self.is_flattenable() && other.is_flattening()
-    }
-
-    pub(crate) fn take_off(&mut self, n: usize) -> Stack {
-        self.content.split_off(self.content.len() - 1 - n).into()
-    }
-
-    pub(crate) fn peek_from_top(&self, n: usize) -> Stack {
-        let new_content = self.content.split_at(self.content.len() - 1- n).1;
-        Vec::from(new_content).into()
-    }
-
-    pub(crate) fn color(&self) -> Option<Color> {
-        self.top().map(|p| p.color)
-    }
-
-    pub(crate) fn len(&self) -> usize {
-        self.content.len()
-    }
-
-    pub fn nth_piece(&self, n: usize) -> Piece {
-        self.content[self.len() - 1 - n]
-    }
-
-    pub fn iter(&self) -> std::slice::Iter<Piece> {
-        self.content.iter()
+    fn valid(&self) -> bool {
+        if self.len() <= 1 {
+            true
+        } else {
+            let tail = &self.content[0..(self.len() - 1)];
+            tail.iter().all(|p| {
+                PieceKind::Stone == p.kind
+            })
+        }
     }
 
 }
 
 impl AddAssign for Stack {
-    fn add_assign(&mut self, other: Stack) {
+    fn add_assign(&mut self, mut other: Stack) {
         assert!(self.compatible_with(&other));
         self.flatten();
-        self.content.extend(other.content.into_iter())
+        self.content.extend(other.content.drain(..))
     }
 }
 
 impl AddAssign<Piece> for Stack {
     fn add_assign(&mut self, other: Piece) {
-        self.content.push(other)
+        self.add_assign(Stack::from(vec![other]));
     }
 }
 
-impl SubAssign<u16> for Stack {
-    fn sub_assign(&mut self, n: u16) {
-        self.content.split_off(self.content.len() - 1 - n as usize);
+impl SubAssign<usize> for Stack {
+    fn sub_assign(&mut self, n: usize) {
+        let _ = self.take_off(n);
     }
 }
 
 impl From<Vec<Piece>> for Stack {
     fn from(v: Vec<Piece>) -> Stack {
-        Stack { content: v }
+        let res = Stack { content: v };
+        assert!(res.valid());
+        res
     }
 }
