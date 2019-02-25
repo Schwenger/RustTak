6e056e78c85f7bf01c8c35d46a044a27321a7263
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2019-02-25 23:09:40 +0100

    Add pseudo main function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,21 @@
+use crate::simulator::Simulator;
+use crate::player::command_line_human::CommandLineHuman;
+use crate::player::Player;
+use crate::player::Color;
+
 mod player;
 mod simulator;
 mod actions;
 mod board;
 mod move_logic;
 mod test_util;
+
+pub fn start() {
+    let red = CommandLineHuman::setup(4, Color::Red, true);
+    let blk= CommandLineHuman::setup(4, Color::Blk, false);
+    let sim = Simulator::new(red, blk, 4);
+    sim.start();
+}
 
 #[cfg(test)]
 mod tests {
